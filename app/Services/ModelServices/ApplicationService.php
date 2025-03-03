c47d58e88dcbaffbfa0279e1cf76c16d4ac78e03
Doston Olimov <dostonolimov61@gmail.com>
2025-03-03 09:20:45 +0500

    03.03.2025 add localtion service file

---
+++ app/Services/ModelServices/ApplicationService.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Services\ModelServices;
+
+use App\Models\Application;
+use App\Repositories\AppStatusChangesReposiroty;
+use App\Repositories\CropDataRepository;
+use App\Repositories\ApplicationRepository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class ApplicationService
+{
+    protected CropDataRepository $cropDataRepository;
+    protected ApplicationRepository $applicationRepository;
+    protected AppStatusChangesReposiroty $appStatusChanges;
+
+
+    public function __construct(
+        CropDataRepository $cropDataRepository,
+        ApplicationRepository $applicationRepository,
+        AppStatusChangesReposiroty $appStatusChanges
+    ) {
+        $this->cropDataRepository = $cropDataRepository;
+        $this->applicationRepository = $applicationRepository;
+        $this->appStatusChanges = $appStatusChanges;
+    }
+
+    public function storeApplication($request)
+    {
+        $user = Auth::user();
+
+        return DB::transaction(function () use ($user, $request) {
+            $crop = $this->cropDataRepository->create([
+                'name_id'       => $request->input('name'),
+                'country_id'    => $request->input('country'),
+                'kodtnved'      => $request->input('tnved'),
+                'party_number'  => $request->input('party_number'),
+                'measure_type'  => $request->input('measure_type'),
+                'amount'        => $request->input('amount'),
+                'year'          => $request->input('year'),
+                'toy_count'     => $request->input('toy_count'),
+                'sxeme_number'  => $request->input('sxeme_number'),
+            ]);
+
+            return $application = $this->applicationRepository->create([
+                'crop_data_id'     => $crop->id,
+                'organization_id'  => $request->input('organization'),
+                'prepared_id'      => $request->input('prepared'),
+                'date'             => formatDate($request->input('dob')),
+                'status'           => Application::STATUS_FINISHED,
+                'data'             => $request->input('data'),
+                'created_by'       => $user->id,
+                'app_type'         => getApplicationType()
+            ]);
+        });
+    }
+
+    public function updateApplication($id,$request)
+    {
+        $app = Application::findOrFail($id);
+        return DB::transaction(function () use ($app, $request) {
+
+            $this->cropDataRepository->update($app->crops->id,
+                [
+                'name_id'       => $request->input('name'),
+                'country_id'    => $request->input('country'),
+                'kodtnved'      => $request->input('tnved'),
+                'party_number'  => $request->input('party_number'),
+                'measure_type'  => $request->input('measure_type'),
+                'amount'        => $request->input('amount'),
+                'year'          => $request->input('year'),
+                'toy_count'     => $request->input('toy_count'),
+                'sxeme_number'  => $request->input('sxeme_number'),
+            ]);
+
+            return $application = $this->applicationRepository->update($app->id,
+                [
+                'organization_id' => $request->input('organization'),
+                'prepared_id'     => $request->input('prepared'),
+                 'date'           => formatDate($request->input('dob')),
+                 'data'           => $request->input('data'),
+            ]);
+        });
+    }
+
+    public function acceptApplication(Application $app)
+    {
+        return $this->applicationRepository->update($app->id, [
+            'status' => Application::STATUS_ACCEPTED,
+            'progress' => Application::PROGRESS_ANSWERED,
+            'accepted_date' => now(),
+            'accepted_id' => Auth::id(),
+        ]);
+    }
+
+    public function rejectApplication(Application $app, string $reason)
+    {
+        DB::transaction(function () use ($app, $reason) {
+            $this->applicationRepository->update($app->id, [
+                'status' => Application::STATUS_REJECTED,
+            ]);
+
+            $this->appStatusChanges::create([
+                'app_id' => $app->id,
+                'status' => Application::STATUS_REJECTED,
+                'comment' => $reason,
+                'user_id' => Auth::id(),
+            ]);
+        });
+    }
+}
